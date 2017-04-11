5049c0f0980e2290042a0e4edda19f96e82d7495
dENQ <qeqedenq@gmail.com>
2017-04-11 20:58:44 +0200

    unos.php update

---
+++ unos.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Unos</title>
+</head>
+<body>
+<div id="table">
+	<form method="POST" action="unos.php">
+		<table>
+
+			 <tr>
+			    <th>Naslov:</th>
+			    <td><input type="text"></input></td>
+			  </tr>
+			  <tr>
+			    <th>Zanr:</th>
+			    <td><input type="text"></input></td>
+			  </tr>
+			  <tr>
+			    <th>Godina:</th>
+			    <td><input type="text"></input></td>
+			  </tr>
+			   <tr>
+			    <th>Trajanje:</th>
+			    <td><input type="text"></input></td>
+			  </tr>
+			   <tr>
+			    <th>Slika:</th>
+			    <td><input type="text"></input></td>
+			  </tr>
+			   <tr>
+			    <th>Gumb:</th>
+			    <td><input type="submit"></input></td>
+			  </tr>
+
+		</table>
+	</form>
+</div>
+
+</body>
+</html>
